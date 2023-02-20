15e8199a180ceef5b5b5775e311177ebfc5893cb
2220372 <2220372>
2023-02-20 14:48:46 +0900

    interface category

--- app/src/main/java/com/example/esupanoru/Icategory.java
+++ app/src/main/java/com/example/esupanoru/Icategory.java
@@ -1,10 +1,19 @@
 package com.example.esupanoru;
 
+import android.os.CpuUsageInfo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface Icategory {
     
     String  categoryName = null;
-    String data[] = new String[0];
-    public void getValues();
+    //各ワードのリスト
+    HashMap<String,Integer> japanese = new HashMap<String ,Integer>();
+    HashMap<String,Integer> spanish = new HashMap<String ,Integer>();
+    //
+    public String getJapanese(int num);
+    public String getSpanish(int num);
     
     
 }
