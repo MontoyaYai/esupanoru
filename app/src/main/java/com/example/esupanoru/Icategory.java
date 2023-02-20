5d385d5300193861c1c0b77f6ca715107f158079
2220372 <2220372>
2023-02-20 14:34:00 +0900

    interface category

---
+++ app/src/main/java/com/example/esupanoru/Icategory.java
@@ -0,0 +1,10 @@
+package com.example.esupanoru;
+
+public interface Icategory {
+    
+    String  categoryName = null;
+    String data[] = new String[0];
+    public void getValues();
+    
+    
+}
