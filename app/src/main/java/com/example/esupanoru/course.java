f00d6d02adf43532969ee3f647a47051487c252a
yai <yai.mecha@gmail.com>
2023-02-20 14:33:19 +0900

    practice layout

--- app/src/main/java/com/example/esupanoru/course.java
+++ app/src/main/java/com/example/esupanoru/course.java
@@ -2,7 +2,10 @@ package com.example.esupanoru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 
 public class course extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class course extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        View practiceImage = findViewById(R.id.practice);
+        View testImage= findViewById(R.id.test);
+
+        practiceImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent practiceIntent= new Intent(getApplicationContext(),Practice.class);
+                startActivity(practiceIntent);
+
+            }
+        });
     }
 }
