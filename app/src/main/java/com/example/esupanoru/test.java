4ffd2c2885b5c4530cf9c0e43cd83a637fbf1f5f
2220372 <2220372>
2023-02-21 16:39:49 +0900

    activitytest

--- app/src/main/java/com/example/esupanoru/test.java
+++ app/src/main/java/com/example/esupanoru/test.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class test extends AppCompatActivity {
 
@@ -12,8 +15,32 @@ public class test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+
         Intent intent = getIntent();
+        //コースの選択　インスタンスの生成
         int option = intent.getIntExtra("OPTION",0);
+        structure course = null;
+
+        switch (option){
+            default:
+            case 0:
+                Toast.makeText(this, "No category selected", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                course = new Greeting();
+                break;
+            case 2:
+                course = new Fruits();
+                break;
+            case 3:
+                course = new Number();
+                break;
+        }
+        //コースをfinalコースに格納
+        structure finalCourse = course;
+
+        TextView testQuestion = findViewById(R.id.test_question);
+
 
     }
 }
