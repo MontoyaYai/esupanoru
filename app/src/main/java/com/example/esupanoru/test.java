5652e0e30ee043dde4555bc3f42cbced2f2a3c14
yai <yai.mecha@gmail.com>
2023-02-21 16:22:25 +0900

    Practice finish final + icon

--- app/src/main/java/com/example/esupanoru/test.java
+++ app/src/main/java/com/example/esupanoru/test.java
@@ -2,6 +2,7 @@ package com.example.esupanoru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class test extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        Intent intent = getIntent();
+        int option = intent.getIntExtra("OPTION",0);
+
     }
 }
