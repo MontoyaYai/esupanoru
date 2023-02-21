7a7f85a9c7534e2489968ffe6e617d608d56e584
2220372 <2220372>
2023-02-21 16:45:05 +0900

    activitytest

--- app/src/main/java/com/example/esupanoru/test.java
+++ app/src/main/java/com/example/esupanoru/test.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class test extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        TextView testQuestion = findViewById(R.id.test_question);
 
+        RadioButton ans1 = findViewById(R.id.answer1);
+        RadioButton ans2 = findViewById(R.id.answer2);
+        RadioButton ans3 = findViewById(R.id.answer3);
         Intent intent = getIntent();
         //コースの選択　インスタンスの生成
         int option = intent.getIntExtra("OPTION",0);
@@ -39,7 +45,7 @@ public class test extends AppCompatActivity {
         //コースをfinalコースに格納
         structure finalCourse = course;
 
-        TextView testQuestion = findViewById(R.id.test_question);
+
 
 
     }
